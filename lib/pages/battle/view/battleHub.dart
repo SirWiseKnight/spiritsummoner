9a39c2d6a64c8ea26d5d7e06e9c16b53eecf40e5
Mason Johnson <masonjohnson77@gmail.com>
2023-07-13 15:15:25 -0500

    Updated Battle Page Layout
    Fixed layout to include Expanded (keep nav buttons on bottom) and to fix right side overflow.

--- lib/pages/battle/view/battleHub.dart
+++ lib/pages/battle/view/battleHub.dart
@@ -9,7 +9,7 @@ import 'package:spirit_summoner/config/barrel.dart';
 const kTextFieldDecoration = InputDecoration(
   hintText: 'Enter a value',
   hintStyle: TextStyle(color: Colors.grey),
-  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
   border: OutlineInputBorder(
     borderRadius: BorderRadius.all(Radius.circular(32.0)),
   ),
@@ -37,94 +37,97 @@ class _BattleScreenState extends State<BattleScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const FlutterLogo(
-              size: 150,
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 4.0),
-              child: Container(
-                child: Text(
-                  'Welcome back, Summoner',
-                  style: GoogleFonts.poppins(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const FlutterLogo(
+                size: 150,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 24.0),
-              child: Container(
-                child: Text(
-                  'Update your username!',
-                  style: GoogleFonts.poppins(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
+              SizedBox(
+                height: 8,
               ),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            TextField(
-                keyboardType: TextInputType.name,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  username = value;
-                  //Do something with the user input.
-                },
-                decoration:
-                    kTextFieldDecoration.copyWith(hintText: 'New Username')),
-            SizedBox(
-              height: 8,
-            ),
-            ElevatedButton(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      child: Icon(FontAwesomeIcons.user),
-                    ),
-                  ),
-                  Text(
-                    'Update Username',
+              Padding(
+                padding: const EdgeInsets.only(bottom: 4.0),
+                child: Container(
+                  child: Text(
+                    'Welcome back, Summoner',
                     style: GoogleFonts.poppins(
-                      fontSize: 16,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                       color: Colors.white,
                     ),
                   ),
-                ],
+                ),
               ),
-              onPressed: () {
-                FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(AuthService().uid)
-                    .update(
-                  {'username': username},
-                );
+              Padding(
+                padding: const EdgeInsets.only(bottom: 24.0),
+                child: Container(
+                  child: Text(
+                    'Update your username!',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              TextField(
+                  keyboardType: TextInputType.name,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    username = value;
+                    //Do something with the user input.
+                  },
+                  decoration:
+                      kTextFieldDecoration.copyWith(hintText: 'New Username')),
+              SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Icon(FontAwesomeIcons.user),
+                      ),
+                    ),
+                    Text(
+                      'Update Username',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                onPressed: () {
+                  FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(AuthService().uid)
+                      .update(
+                    {'username': username},
+                  );
 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()),
-                );
-              },
-            ),
-            BottomNavBubbles(),
-          ],
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                  );
+                },
+              ),
+              Expanded(child: Container()),
+              BottomNavBubbles(),
+            ],
+          ),
         ),
       ),
     );
