72c0fe0dfc413cba5079f35dac3052e378431fba
Mason Johnson <masonjohnson77@gmail.com>
2023-07-23 22:28:18 -0500

    Updated Battle Function
    Now describes turns more accurately. Still needs updates to better prioritize faster monsters - work in progress.

--- functions/battle-scene-wip.jsx
+++ functions/battle-scene-wip.jsx
@@ -105,71 +105,93 @@ class Battle {
   constructor(team1, team2) {
     this.team1 = team1;
     this.team2 = team2;
+    this.turnNumber = 0; // Initialize the turn number to 0
   }
 
 // method to simulate a round of battle
-battleRound() {
-  let index1 = 0;
-  let index2 = 0;
+  battleRound() {
+    this.turnNumber++; // Increment the turn number at the start of each round
+    console.log(``); 
+    console.log(`--- Turn ${this.turnNumber} ---`); // Print the current turn number
 
-  // Continue the battle until all Pokémon on one team have fainted
-  while (index1 < this.team1.length && index2 < this.team2.length) {
-    let attacker1 = this.team1[index1];
-    let defender1 = this.team2[index1];
+    this.turnNumber = 1;
+    let index1 = 0;
+    let index2 = 0;
 
-    if (attacker1.health > 0) {
-      console.log(`${attacker1.name} attacks ${defender1.name}`);
+    // Continue the battle until all Pokémon on one team have fainted
+    while (index1 < this.team1.length && index2 < this.team2.length) {
+      let attacker1 = this.team1[index1];
+      let defender1 = this.team2[index1]; 
 
-      let bestMove1 = attacker1.chooseBestMove(defender1);
-      let numAttacks1 = attacker1.speed / defender1.speed >= 3 ? 3 : attacker1.speed / defender1.speed >= 2 ? 2 : 1;
+      if (attacker1.health > 0) {
+        //console.log(`${attacker1.name} attacks ${defender1.name}`);
 
-      for (let j = 0; j < numAttacks1; j++) {
-        let damage1 = attacker1.calculateMoveDamage(bestMove1, defender1);
-        defender1.takeDamage(damage1);
-        console.log(`${attacker1.name} uses ${bestMove1.name}, dealing ${damage1} damage to ${defender1.name}`);
-        console.log(`${defender1.name} has ${defender1.health} health remaining`);
+        let bestMove1 = attacker1.chooseBestMove(defender1);
+        let numAttacks1 = attacker1.speed / defender1.speed >= 3 ? 3 : attacker1.speed / defender1.speed >= 2 ? 2 : 1; // 
 
-        // Check if defender1's health is 0 or less and remove from the team if true
-        if (defender1.health <= 0) {
-          console.log(`${defender1.name} has fainted and is removed from Team 2`);
-          this.team2.splice(index1, 1);
-          //index1++;
-          // No need to break here; we want the same attacker to attack again
+        for (let j = 0; j < numAttacks1; j++) {
+          // Check if the attacker is still alive before proceeding with the attack
+          if (attacker1.health > 0) {
+            let damage1 = attacker1.calculateMoveDamage(bestMove1, defender1);
+            defender1.takeDamage(damage1);
+            console.log(`${attacker1.name} uses ${bestMove1.name}, dealing ${damage1} damage to ${defender1.name}`);
+            console.log(`${defender1.name} has ${defender1.health} health remaining`);
+  
+            // Check if defender1's health is 0 or less and remove from the team if true
+            if (defender1.health <= 0) {
+              this.turnNumber = this.turnNumber + 1; // Increment the turn number at the start of each round
+              console.log(``); 
+              console.log(`--- Turn ${this.turnNumber} ---`); // Print the current turn number
+              console.log(`${defender1.name} has fainted and is removed from Team 2`);
+              this.team2.splice(index1, 1);
+              break; // Exit the loop if the defender faints during the turn
+            }
+          }
+        }
+
+      // Check if both teams still have Pokemon available to battle
+      if (index2 >= this.team2.length) {
+        break;
+      }
+
+      this.turnNumber = this.turnNumber + 1; // Increment the turn number at the start of each round
+      console.log(``); 
+      console.log(`--- Turn ${this.turnNumber} ---`); // Print the current turn number
+
+      let attacker2 = this.team2[index2];
+      let defender2 = this.team1[index2];
+
+      if (attacker2.health > 0) {
+        //console.log(`${attacker2.name} attacks ${defender2.name}`);
+
+        let bestMove2 = attacker2.chooseBestMove(defender2);
+        let numAttacks2 = attacker2.speed / defender2.speed >= 3 ? 3 : attacker2.speed / defender2.speed >= 2 ? 2 : 1; // 
+
+        for (let j = 0; j < numAttacks2; j++) {
+          // Check if the attacker is still alive before proceeding with the attack
+          if (attacker2.health > 0) {
+            let damage2 = attacker2.calculateMoveDamage(bestMove2, defender2);
+            defender2.takeDamage(damage2);
+            console.log(`${attacker2.name} uses ${bestMove2.name}, dealing ${damage2} damage to ${defender2.name}`);
+            console.log(`${defender2.name} has ${defender2.health} health remaining`);
+  
+            // Check if defender2's health is 0 or less and remove from the team if true
+            if (defender2.health <= 0) {
+              this.turnNumber = this.turnNumber + 1; // Increment the turn number at the start of each round
+              console.log(``); 
+              console.log(`--- Turn ${this.turnNumber} ---`); // Print the current turn number
+              console.log(`${defender2.name} has fainted and is removed from Team 1`);
+              this.team1.splice(index2, 1);
+              break; // Exit the loop if the defender faints during the turn
+            }
+
+    this.turnNumber = this.turnNumber + 1; // Increment the turn number at the start of each round
+    console.log(``); 
+    console.log(`--- Turn ${this.turnNumber} ---`); // Print the current turn number
+          }
         }
       }
     }
-
-
-    // Check if both teams still have Pokemon available to battle
-    if (index2 >= this.team2.length) {
-      break;
-    }
-
-    let attacker2 = this.team2[index2];
-    let defender2 = this.team1[index2];
-
-    if (attacker2.health > 0) {
-      console.log(`${attacker2.name} attacks ${defender2.name}`);
-
-      let bestMove2 = attacker2.chooseBestMove(defender2);
-      let numAttacks2 = attacker2.speed / defender2.speed >= 3 ? 3 : attacker2.speed / defender2.speed >= 2 ? 2 : 1;
-
-      for (let j = 0; j < numAttacks2; j++) {
-        let damage2 = attacker2.calculateMoveDamage(bestMove2, defender2);
-        defender2.takeDamage(damage2);
-        console.log(`${attacker2.name} uses ${bestMove2.name}, dealing ${damage2} damage to ${defender2.name}`);
-        console.log(`${defender2.name} has ${defender2.health} health remaining`);
-
-        // Check if defender2's health is 0 or less and remove from the team if true
-        if (defender2.health <= 0) {
-          console.log(`${defender2.name} has fainted and is removed from Team 1`);
-          this.team1.splice(index2, 1);
-          //index2++;
-          // No need to break here; we want the same attacker to attack again
-        }
-      }
-    }
-
   }
 }
 
@@ -230,11 +252,11 @@ let slashM= new Move("Slash","Normal","Physical" ,70 );
 let nightSlash= new Move("Night Slash","Dark","Physical" ,70 );
 
 // create some Pokemon
-let pikachu= new Pokemon("Pikachu",100 ,50 ,30 ,40 ,20 ,90 ,"Static","Light Ball",[thunderbolt,quickAttack,ironTail,voltTackle]);
+let pikachu= new Pokemon("Pikachu",100 ,50 ,30 ,40 ,20 ,30 ,"Static","Light Ball",[thunderbolt,quickAttack,ironTail,voltTackle]);
 let charmander= new Pokemon("Charmander",80 ,60 ,20 ,50 ,30 ,60 ,"Blaze","Charcoal",[ember,scratch,dragonRage,fireFang]);
 let squirtle= new Pokemon("Squirtle",90 ,40 ,40 ,30 ,50 ,70 ,"Torrent","Mystic Water",[waterGun,tackle,bubble,bite]);
 
-let bulbasaur= new Pokemon("Bulbasaur",85 ,55 ,35 ,45 ,25 ,75 ,"Overgrow","Miracle Seed",[vineWhip,razorLeaf,seedBomb,solarBeam]);
+let bulbasaur= new Pokemon("Bulbasaur",81 ,55 ,35 ,45 ,25 ,75 ,"Overgrow","Miracle Seed",[vineWhip,razorLeaf,seedBomb,solarBeam]);
 let jigglypuff= new Pokemon("Jigglypuff",95 ,45 ,25 ,35 ,45 ,65 ,"Cute Charm","Moon Stone",[pound,bodySlam,doubleSlap,hyperVoice]);
 let meowth= new Pokemon("Meowth",70 ,65 ,15 ,55 ,35 ,80 ,"Pickup","Amulet Coin",[scratchM,furySwipes,slashM,nightSlash]);
 
@@ -252,9 +274,17 @@ battle.battleRound();
 
 // determine the winner
 if (team1.every(pokemon => pokemon.health <=0)) {
-console.log("Team2 wins!");
+  console.log("");
+  console.log("--- RESULT ---");
+  console.log("Team2 wins!");
+  console.log("");
+  console.log("-- REWARDS ---");
+  console.log("List rewards here!");
 } else if (team2.every(pokemon => pokemon.health <=0)) {
+  console.log("");
+  console.log("--- RESULT ---");
 console.log("Team1 wins!");
+console.log("");
+console.log("-- REWARDS ---");
+console.log("List rewards here!");
 }
-
-// need to have each step of the battle write itself out to return to the app via JSON
